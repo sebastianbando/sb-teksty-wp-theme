e553360dda15d53aef2d1651ce4d7cc17eb0431f
Sebastian Bańdo <sydbeat@gmail.com>
2017-12-13 16:50:00 +0100

    adding header.php and footer.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo get_bloginfo(); ?></title>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	</head>
+	<body>
+		<header>
+			<img class="logo" src="<?php echo get_template_directory_uri(); ?>/logo.png">
+			<h1><?php echo get_bloginfo(); ?></h1>
+		</header>
