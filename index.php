3236e5dfa31038b7e3b6cbada0d07cc7fe2ed865
Sebastian Bańdo <sydbeat@gmail.com>
2017-12-13 15:39:03 +0100

    updated index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo get_bloginfo(); ?></title>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	</head>
+	<body>
+		<header>
+		</header>
+		<main>
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
