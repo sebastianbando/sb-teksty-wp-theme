1b4fc49987334cd9339ab71fca1219d263a1e8a7
Sebastian Bańdo <sydbeat@gmail.com>
2017-12-13 16:53:12 +0100

    index-php update according to wp header and footer structure

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title><?php echo get_bloginfo(); ?></title>
-		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
-	</head>
-	<body>
-		<header>
-		</header>
+<?php get_header(); ?>
+
 		<main>
+			
 		</main>
-		<footer>
-		</footer>
-	</body>
-</html>
+		
+<?php get_footer(); ?>
